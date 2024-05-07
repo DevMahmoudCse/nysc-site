e5a110975bb9e2ac4ce5e341d542cabf8f5762d9
Mahmoud Muhammad Sani <sanimahmoud10@gmail.com>
2024-05-06 17:52:09 -0700

    db updated

--- NYSC/db/nysc.sql
+++ NYSC/db/nysc.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 13, 2024 at 09:21 AM
--- Server version: 10.1.33-MariaDB
--- PHP Version: 7.2.6
+-- Generation Time: May 07, 2024 at 02:51 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -32,7 +31,7 @@ CREATE TABLE `admin` (
   `id` int(11) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `admin`
@@ -51,7 +50,7 @@ CREATE TABLE `batch` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `year` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `batch`
@@ -64,6 +63,53 @@ INSERT INTO `batch` (`id`, `name`, `year`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `cds`
+--
+
+CREATE TABLE `cds` (
+  `id` int(11) NOT NULL,
+  `batch` varchar(100) NOT NULL,
+  `month` varchar(100) NOT NULL,
+  `venue` varchar(500) NOT NULL,
+  `date` varchar(11) NOT NULL,
+  `time` varchar(100) NOT NULL,
+  `year` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cds`
+--
+
+INSERT INTO `cds` (`id`, `batch`, `month`, `venue`, `date`, `time`, `year`) VALUES
+(2, 'Group Tiger', 'April', 'Kano Central Hotel', '2024-04-30', '08:00 AM', 2024);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clearance`
+--
+
+CREATE TABLE `clearance` (
+  `id` int(11) NOT NULL,
+  `batch` varchar(100) NOT NULL,
+  `month` varchar(100) NOT NULL,
+  `year` int(11) NOT NULL,
+  `date` varchar(100) NOT NULL,
+  `time` varchar(100) NOT NULL,
+  `info` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `clearance`
+--
+
+INSERT INTO `clearance` (`id`, `batch`, `month`, `year`, `date`, `time`, `info`) VALUES
+(1, 'Batch A Stream 1', 'April', 2024, '2024-05-11', '08:30 AM', 'Come on Time'),
+(2, 'Batch A Stream 2', 'April', 2024, '2024-05-11', '08:00 AM', 'Come on time');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `corpers`
 --
 
@@ -71,18 +117,40 @@ CREATE TABLE `corpers` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
   `gender` varchar(100) NOT NULL,
   `batch` varchar(50) NOT NULL,
+  `groups` varchar(100) NOT NULL,
   `lga` varchar(200) NOT NULL,
   `state` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `corpers`
 --
 
-INSERT INTO `corpers` (`id`, `name`, `email`, `gender`, `batch`, `lga`, `state`) VALUES
-(1, 'Mahmoud Muhammad Sani', 'sanimahmoud10@gmail.com', 'male', 'Batch A Stream 2', 'Nasarawa', 'kano');
+INSERT INTO `corpers` (`id`, `name`, `email`, `password`, `gender`, `batch`, `groups`, `lga`, `state`) VALUES
+(1, 'Mahmoud Muhammad Sani', 'sanimahmoud10@gmail.com', '3c6f93ffa50a9ff9d6bdef400a55aed4', 'male', 'Batch A Stream 2', 'Group Gidan Maza\r\n', 'Nasarawa', 'kano'),
+(2, 'Salman Yahya', 'salmanyahya@gmail.com', '3c6f93ffa50a9ff9d6bdef400a55aed4', 'male', 'Batch A Stream 2', 'Group Tiger', 'Dutse', 'jigawa');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `groups`
+--
+
+CREATE TABLE `groups` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `groups`
+--
+
+INSERT INTO `groups` (`id`, `name`) VALUES
+(3, 'Group Tiger'),
+(4, 'Group Gidan Maza');
 
 --
 -- Indexes for dumped tables
@@ -101,9 +169,27 @@ ALTER TABLE `batch`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `cds`
+--
+ALTER TABLE `cds`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `clearance`
+--
+ALTER TABLE `clearance`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `corpers`
 --
 ALTER TABLE `corpers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `groups`
+--
+ALTER TABLE `groups`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -123,10 +209,28 @@ ALTER TABLE `batch`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT for table `cds`
+--
+ALTER TABLE `cds`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `clearance`
+--
+ALTER TABLE `clearance`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `corpers`
 --
 ALTER TABLE `corpers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `groups`
+--
+ALTER TABLE `groups`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
